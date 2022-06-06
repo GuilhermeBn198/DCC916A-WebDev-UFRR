c14f47353ead4b2946b51731ab76d4b42b2cd60a
Guilherme Bernardo <bguilherme51@gmail.com>
2022-06-06 16:53:31 -0300

    80% implementation of the php formulary

---
+++ Aula_06-06/inserindo.php
@@ -0,0 +1,19 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inserir no BD</title>
+</head>
+<body>
+    <? /** Arquivo para conectar ao banco de dados */
+    require("conectdb.php");
+
+    /** Pegando a data */
+    $data = date('Y-m-d');
+    /** Pegando a hora */
+    $hora = strftime("%H:%M:%S");
+
+    /** Inserindo na tabela tb_mural, nos campos os valores das variaveis */
+</body>
+</html>
